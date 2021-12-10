649952122fd17556ab18fd3c21235af5007a189f
Giuco <giuco.ferrari@gmail.com>
2021-12-10 19:53:53 -0300

    lesson 27

--- section3/main.go
+++ section3/main.go
@@ -1,11 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
 	cards, hand := cards.drawHand(5)
-	cards.print()
-	hand.print()
-	fmt.Println(hand.toString())
+	hand.saveToFile("hand.txt")
 }
