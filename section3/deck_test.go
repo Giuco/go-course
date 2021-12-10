cc87ba9bb7200f94178333b47ec665a728941469
Giuco <giuco.ferrari@gmail.com>
2021-12-10 20:54:45 -0300

    lesson 33

--- section3/deck_test.go
+++ section3/deck_test.go
@@ -1,1 +1,12 @@
 package main
+
+import "testing"
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+	expected_len := 52
+
+	if len(d) != expected_len {
+		t.Errorf("Expected deck size of %v but got %v", expected_len, len(d))
+	}
+}
