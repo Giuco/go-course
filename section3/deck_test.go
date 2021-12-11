2b1ee8a66c8d7754c74bac4492aa05192356e6f1
Giuco <giuco.ferrari@gmail.com>
2021-12-10 21:02:17 -0300

    lesson 34

--- section3/deck_test.go
+++ section3/deck_test.go
@@ -9,4 +9,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != expected_len {
 		t.Errorf("Expected deck size of %v but got %v", expected_len, len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card to be 'Ace of Spades', got %v", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected last card to be 'King of Clubs', go %v", d[len(d)-1])
+	}
 }
