ed2f045a2a0206f6f08726e67fcb96f4ad24328f
Giuliano Ferrari <giucoferrari@gmail.com>
2021-12-31 12:08:45 -0300

    lesson 50

--- section5/main.go
+++ section5/main.go
@@ -5,9 +5,20 @@ import (
 )
 
 func main() {
+	// var colors map[string]string
+
+	// colors := make(map[string]string)
+
 	colors := map[string]string{
 		"red":   "#FF0000",
 		"green": "#00FF00",
 	}
+
+	fmt.Println(colors)
+
+	colors["white"] = "#FFFFFF"
+	fmt.Println(colors)
+
+	delete(colors, "red")
 	fmt.Println(colors)
 }
