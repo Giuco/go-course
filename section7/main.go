a5168a751e3944c9656012ec5321f62a8f8493f7
Giuliano Ferrari <giucoferrari@gmail.com>
2022-01-03 19:48:04 -0300

    lesson 75

--- section7/main.go
+++ section7/main.go
@@ -14,18 +14,23 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, url := range urls {
-		checkURL(url)
+		go checkURL(url, c)
 	}
 
+	fmt.Println(<-c)
 }
 
-func checkURL(url string) {
+func checkURL(url string, c chan string) {
 	_, err := http.Get(url)
 	if err != nil {
 		fmt.Println(url, "might be down")
+		c <- "Down"
 		return
 	}
 
 	fmt.Println(url, "is up")
+	c <- "Up"
 }
